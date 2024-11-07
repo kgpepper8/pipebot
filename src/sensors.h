cc8f2c54864d8fb59b400391ee5dd3646290192b
Kiran Ghanekar <kgpepper8@gmail.com>
2024-11-07 17:17:00 -0500

    Added sensors.h to hold all sensor and motor locations.

---
+++ src/sensors.h
@@ -0,0 +1,15 @@
+//List of all sensor locations.
+//TO BE UPDATED!
+
+#ifndef SENSORS
+	#define SENSORS
+
+	const tSensors USPORT = S1;
+	const tSensors ACCPORT = S2;
+	const tSensors TOUCHPORT = S3;
+
+	const tMotor FDRIVE = motorA;
+	const tMotor RDRIVE = motorB;
+	const tMotor BRUSH = motorC;
+	const tMotor LDSCREW = motorD;
+#endif
